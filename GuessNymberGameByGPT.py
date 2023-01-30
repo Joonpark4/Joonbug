4433c372a3112ca5be75676279b19ecea2810a8b
Joon Park <sargadil@gmail.com>
2023-01-31 00:34:22 +0100

    230131_4

--- GuessNymberGameByGPT.py
+++ GuessNymberGameByGPT.py
@@ -11,34 +11,40 @@ def number_guessing_game():
     ai_min = 1
     ai_max = 100
     
+    user_turn = True
+    
     while True:
-        user_guess = int(input("Enter your guess: "))
+        if user_turn:
+            user_guess = int(input("Enter your guess: "))
+            
+            if user_guess == number:
+                print(f"You win! The number was {number}. You took {attempts} attempts.")
+                break
+            
+            elif user_guess < number:
+                print("Your guess is lower.")
+                user_min = user_guess + 1
+                ai_min = max(ai_min, user_min)
+            elif user_guess > number:
+                print("Your guess is higher.")
+                user_max = user_guess - 1
+                ai_max = min(ai_max, user_max)
+        else:
+            ai_guess = random.randint(ai_min, ai_max)
+            print(f"AI guessed {ai_guess}")
+            
+            if ai_guess == number:
+                print(f"AI wins! The number was {number}. AI took {attempts} attempts.")
+                break
+            
+            elif ai_guess < number:
+                print("AI's guess is lower.")
+                ai_min = ai_guess + 1
+            elif ai_guess > number:
+                print("AI's guess is higher.")
+                ai_max = ai_guess - 1
         
-        if user_guess == number:
-            print(f"You win! The number was {number}. You took {attempts} attempts.")
-            break
-        
-        elif user_guess < number:
-            print("Your guess is lower.")
-            user_min = user_guess + 1
-        elif user_guess > number:
-            print("Your guess is higher.")
-            user_max = user_guess - 1
-        
-        ai_guess = random.randint(ai_min, ai_max)
-        print(f"AI guessed {ai_guess}")
-        
-        if ai_guess == number:
-            print(f"AI wins! The number was {number}. AI took {attempts} attempts.")
-            break
-        
-        elif ai_guess < number:
-            print("AI's guess is lower.")
-            ai_min = ai_guess + 1
-        elif ai_guess > number:
-            print("AI's guess is higher.")
-            ai_max = ai_guess - 1
-        
+        user_turn = not user_turn
         attempts += 1
 
 number_guessing_game()
